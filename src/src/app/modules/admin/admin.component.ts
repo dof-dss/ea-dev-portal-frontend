3c3f944c51744a17ea96fa64f663d20022d25991
GrahamMcVea-dof <55088706+GrahamMcVea-dof@users.noreply.github.com>
2019-12-19 10:44:02 +0000

    Add placeholder to admin html

--- src/src/app/modules/admin/admin.component.ts
+++ src/src/app/modules/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-admin',
@@ -6,6 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    scales: { xAxes: [{}], yAxes: [{}] },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      }
+    }
+  };
+  public barChartLabels: Label[] = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ];
+  public barChartType: ChartType = 'line';
+  public barChartLegend = true;
+
+  public barChartData: ChartDataSets[] = [{
+    data: [
+      15339,
+      21345,
+      18483,
+      24003,
+      23489,
+      24092,
+      12034
+    ],
+    lineTension: 0,
+    backgroundColor: 'transparent',
+    borderColor: '#007bff',
+    borderWidth: 4,
+    pointBackgroundColor: '#007bff',
+    label: 'Total Usage'
+  }];
 
   constructor() { }
 
